c82142d3820e422bfcebbeb7ffeb2573a2b99ba0
kanozec <kanozec@gmail.com>
2018-11-02 01:08:20 +0800

    Get the <a> node

--- link_parser/parse.go
+++ link_parser/parse.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strings"
+
+	"golang.org/x/net/html"
 )
 
 type Link struct {
@@ -16,4 +19,33 @@ func main() {
 		fmt.Print(err)
 	}
 	fmt.Println(string(data))
+
+	r := strings.NewReader(string(data))
+	doc, err := html.Parse(r)
+	if err != nil {
+		fmt.Print(err)
+	}
+	fmt.Println(doc.Type)
+	if doc.Type == html.ElementNode && doc.Data == "a" {
+		fmt.Println([]*html.Node{doc})
+	}
+
+	fmt.Println(doc.FirstChild.NextSibling)
+
+	nodes := linkNodes(doc)
+
+	for _, node := range nodes {
+		fmt.Println(node)
+	}
+}
+
+func linkNodes(n *html.Node) []*html.Node {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
+	}
+	var ret []*html.Node
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret = append(ret, linkNodes(c)...)
+	}
+	return ret
 }
