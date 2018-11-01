cd37b5cd2d36f89a72e85ca49f8e8f70f9707cf4
kanozec <kanozec@gmail.com>
2018-11-02 01:04:31 +0800

    Test for phone normalizer

--- phone/normalize.go
+++ phone/normalize.go
@@ -1,28 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"regexp"
 )
-
-type example struct {
-	input  string
-	expect string
-}
 
 func normalize(phone string) string {
 	re := regexp.MustCompile("\\D")
 	return re.ReplaceAllString(phone, "")
-}
-
-func main() {
-	exs := []example{
-		{"1234567890", "1234567890"},
-		{"(123) 456 7890", "1234567890"},
-	}
-
-	for _, ex := range exs {
-		fmt.Println(normalize(ex.input))
-		fmt.Println(ex.expect)
-	}
 }
--- phone/normalizer_test.go
+++ phone/normalizer_test.go
@@ -1,1 +1,28 @@
 package main
+
+import "testing"
+
+type testSample struct {
+	input  string
+	expect string
+}
+
+func TestNormalize(t *testing.T) {
+	cases := []testSample{
+		{"0123456789", "0123456789"},
+		{"418 456 7891", "4184567891"},
+		{"(225) 456 7892", "2254567892"},
+		{"(523) 476-7893", "5234767893"},
+		{"723-656-7894", "7236567894"},
+		{"(123)555-2987", "1235552987"},
+	}
+
+	for _, c := range cases {
+		t.Run(c.input, func(t *testing.T) {
+			result := normalize(c.input)
+			if result != c.expect {
+				t.Errorf("got %s; want %s", result, c.expect)
+			}
+		})
+	}
+}
